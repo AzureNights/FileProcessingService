ac76afa85fbb84822aca6e110f704c9b99525237
AzureNights <kokilavanielamparidhi@gmail.com>
2025-09-26 17:09:41 +0800

    Included logger and info and warning

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
 import os
 from flask import Flask, request, jsonify
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
 @app.route("/")
 def check():
+    logger.info("Service is running.")
     return "File Processing Service is running!"
 
 @app.route("/upload-file", methods=["POST"])
 def upload_file():
+    logger.info("Upload file function is running.")
+
     if "file" not in request.files:
+        logger.warning("There was no file uploaded.")
         return jsonify({"error": "No file uploaded"}), 400
     
     file = request.files["file"]
 
     if file.filename == "":
+        logger.warning("No file name found")
         return jsonify({"error": "File not found"}), 400
     
+    logger.info(f"File '{file.filename}' was uploaded successfully.")
     return jsonify({
         "message": "File uploaded successfully!",
         "filename": file.filename,
